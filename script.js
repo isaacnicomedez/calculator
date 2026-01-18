f42043dac5fde2281d20d29145433247ab346acb
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 07:22:55 -0800

    Initialize arithmetic functionality

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
-const n1 = 0, n2 = 0, operator = "";
-
-function add(n1, n2) {
-    return n1 + n2;
-}
-
-function subtract(n1, n2) {
-    return n1 - n2;
-}
-
-function multiply(n1, n2) {
-    return n1 * n2;
-}
-
-function divide(n1, n2) {
-    return n1 / n2;
-}
-
 function operate(operator, n1, n2) {
-    if (operator === "+") {
-        add(n1, n2);
-    } else if (operator === "-") {
-        subtract(n1 - n2);
-    } else if (operator === "*") {
-        multiply(n1, n2);
-    } else if (operator === "/") {
-        divide(n1, n2);
+    const add = (n1, n2) => n1 + n2;
+    const subtract = (n1, n2) => n1 - n2;
+    const multiply = (n1, n2) => n1 * n2;
+    const divide = (n1, n2) => n2 === 0 ? 'ERROR Division by Zero!' : n1 / n2;
+
+    switch (operator) {
+        case "+":
+            return add(n1, n2);
+        case "-":
+            return subtract(n1, n2);
+        case "*":
+            return multiply(n1, n2);
+        case "/":
+            return divide(n1, n2);
     }
 }
+
+console.log(operate('*', 10, 10));
