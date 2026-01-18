137df80bc5513219f12718feb0f86d8fb4258e29
Isaac <isaacnicomedez1@gmail.com>
2026-01-18 06:18:14 -0800

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const n1 = 0, n2 = 0, operator = "";
+
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -12,4 +14,16 @@ function multiply(n1, n2) {
 
 function divide(n1, n2) {
     return n1 / n2;
+}
+
+function operate(operator, n1, n2) {
+    if (operator === "+") {
+        add(n1, n2);
+    } else if (operator === "-") {
+        subtract(n1 - n2);
+    } else if (operator === "*") {
+        multiply(n1, n2);
+    } else if (operator === "/") {
+        divide(n1, n2);
+    }
 }
