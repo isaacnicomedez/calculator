676b63afa88ddae054a272983c972891d8c11d7d
Isaac <isaacnicomedez1@gmail.com>
2026-01-21 00:23:28 -0800

    Add operation function

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
     return n1 / n2;
 }
+
+function operate(operator, n1, n2) {
+    switch (operator) {
+        case "+":
+            return add(n1, n2);
+        case "+":
+            return add(n1, n2);
+        case "&multiply":
+            return multiply(n1, n2);
+        case "&divide":
+            return divide(n1, n2);
+    }
+}
+
+console.log(operate("&multiply", 2, 2));
