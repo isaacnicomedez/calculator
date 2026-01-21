41ce33e4566b5e4300d5e852fcd6bbba88c54233
Isaac <isaacnicomedez1@gmail.com>
2026-01-21 02:59:39 -0800

    Add working reducer

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function divide(n1, n2) {
 
     return result;
 }
+
+let expression = [10, "+", 11, "*", 13, "-", 14, "/", "2"];
+
+expression.map((val, i) => {
+    if (val === "*" || val === "/") {
+        const prev = i - 1;
+        const next = i + 1;
+        if (val === "*") {
+            expression.splice(prev, 3, expression[prev] * expression[next]);
+        } else if (val === "/") {
+            expression.splice(prev, 3, expression[prev] / expression[next]);
+        }
+    }
+});
+
+console.log(expression);
