ae75673ebb011369c33a1af1922af2bc4872b46b
Isaac <isaacnicomedez1@gmail.com>
2026-01-30 03:56:44 -0800

    Add testing array and ordered operators array

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
 function add(n1, n2) {
-    return n1 + n2;
+    return parseFloat(n1) + parseFloat(n2);
 }
+
 function subtract(n1, n2) {
-    return n1 - n2;
+    return parseFloat(n1)- parseFloat(n2);
 }
 
 function multiply(n1, n2) {
-    return n1 * n2;
+    return parseFloat(n1) * parseFloat(n2);
 }
 
 function divide(n1, n2) {
-    return n1 / n2;
+    return parseFloat(n1) / parseFloat(n2);
 }
 
 
 function operate() {
-    let expression = [10, "/", 0];
-    const operators = [["*", "/"], ["+", "-"]];
-
-    const calculated = operators.forEach(operator => {
-        let i = 0;
-
-        while (expression.some(val => operator.includes(val))) {
-            let answer = 0;
-            const current = expression[i];
-            const prev = expression[i-1];
-            const next = expression[i+1];
-
-            if (operator.includes(current)) {
-                if (current === "*") {
-                    answer = multiply(prev, next);
-                } else if (current === "/") {
-                    if (next === 0) return "ZERO DIVISION ERROR!";
-                    answer = divide(prev, next);
-                } else if (current === "+") {
-                    answer = add(prev, next);
-                } else if (current === "-") {
-                    answer = subtract(prev, next);
-                }
-                expression.splice(i-1, 3, answer);
-                i = 0;
-            } else {
-                if (i > expression.length) i = 0;
-                i++;
-            }
-        }
-    });
-
-    console.log(expression);
+    let expression = [10, "/", 1, "*", 1, "+", 8, "-", 2, "/", 2];
+    const operators = [["*","/"],["+","-"]];
 }
+
 
 function handleClicks() {
     const expression = [];
@@ -72,4 +43,3 @@ function handleClicks() {
     });
 }
 
-console.log(operate());
